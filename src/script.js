026a34428dad7c79661612dd2719c2e268b5d2d2
mizunomi <mizuno@miccchi.com>
2018-08-22 20:48:16 +0900

    debug表示除去

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
-
 var imgURL = chrome.extension.getURL("images/notebook.svg");
 $("td.content span a[title*='ipynb']").each(function(index, element){
-   // console.log(element.attr('href'));
    var url = element.href.replace("https://github.com", "https://colab.research.google.com/github");
-   console.log(url);
    $(this).parent().prepend('<a href="'+url+'" target="_blank"><img src="'+imgURL+'" width="18" height="18"/></a>');
  });
  if ($("strong.final-path")){
@@ -11,7 +8,5 @@ $("td.content span a[title*='ipynb']").each(function(index, element){
    if (/.ipynb$/.test(url)){
      url = url.replace("https://github.com", "https://colab.research.google.com/github");
      $("div.breadcrumb").parent().prepend('<a href="'+url+'" target="_blank"><img src="'+imgURL+'" width="18" height="18"/></a>');
-     console.log(url);
    }
-
  }
